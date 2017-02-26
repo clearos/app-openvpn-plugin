30cd8154abe21ea13ba074e45dbfcdd00a86b5d4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:54:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'Politicile OpenVPN asigură un control de acces pentru aplicația de server OpenVPN.';
+$lang['openvpn_plugin_app_name'] = 'Politici OpenVPN';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN utilizator';
