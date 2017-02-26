fe77e9d1173fd1041f1d808e2262b36bdbd18297
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:55:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN Maano fana phihlello ya taolo ya bakeng sa app e OpenVPN seva.';
+$lang['openvpn_plugin_app_name'] = 'OpenVPN Maano';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN User';
