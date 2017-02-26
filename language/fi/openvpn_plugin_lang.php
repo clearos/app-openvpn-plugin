a9ed26e3b5befe6dd959b67c215dc258f2427857
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:49:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN säännöt tarjoavat kulunvalvonta, että OpenVPN palvelimen sovellus.';
+$lang['openvpn_plugin_app_name'] = 'OpenVPN säännöt';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN Käyttäjä';
