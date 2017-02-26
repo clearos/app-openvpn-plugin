a4c2ea985caeed6f523135707c33a0f80404f32c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:50:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'a chur ar fáil Polasaithe OpenVPN rialú rochtana don app fhreastalaí OpenVPN.';
+$lang['openvpn_plugin_app_name'] = 'Polasaithe OpenVPN';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN Úsáideoir';
