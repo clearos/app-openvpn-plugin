d513acaf89670a392e59237c2c6bf1222093b8b2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:48:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN的政策为OpenVPN服务器应用程序的访问控制。';
+$lang['openvpn_plugin_app_name'] = 'OpenVPN的政策';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN的用户';
