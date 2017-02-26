1613b9606e40ea6afbfffb88f003e5e1e3115074
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:47:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN քաղաքականությունը ապահովում է մուտքի վերահսկման համար openvpn սերվեր հավելվածի:';
+$lang['openvpn_plugin_app_name'] = 'openVPN քաղաքականություն';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN Օգտվող';
