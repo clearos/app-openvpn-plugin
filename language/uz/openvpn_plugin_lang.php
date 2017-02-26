766249fe3576e10a20680690ee8866bd6c700d37
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:58:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN Siyosatlar OpenVPN server ilova uchun erkin foydalanish nazorat beradi.';
+$lang['openvpn_plugin_app_name'] = 'OpenVPN siyosat';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN Foydalanuvchi';
