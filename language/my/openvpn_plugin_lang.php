ee5fb5706d0733f64996a492cf7cbac6aa9412fb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:48:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN ကိုမူဝါဒများအ OpenVPN ကို server ကို app ကိုများအတွက်ဝင်ရောက်ခွင့်ကိုထိန်းချုပ်ပေး။';
+$lang['openvpn_plugin_app_name'] = 'OpenVPN ကိုမူဝါဒများ';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN ကိုအသုံးပြုသူတို့၏';
