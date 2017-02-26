c7958963a0395ffe88a9358853ffbb6082dee735
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:56:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN கொள்கைகள் OpenVPN சர்வர் பயன்பாட்டை கட்டுப்படுத்த உதவும்.';
+$lang['openvpn_plugin_app_name'] = 'OpenVPN கொள்கைகள்';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN பயனர்';
