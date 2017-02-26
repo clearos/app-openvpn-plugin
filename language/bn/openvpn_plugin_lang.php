609d10ce8ce8f980ec232e7bfa9effb83895b9c1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:47:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'VPN খুলুন নীতিসমূহ VPN খুলুন সার্ভার অ্যাপ্লিকেশন জন্য প্রবেশাধিকার নিয়ন্ত্রণ প্রদান.';
+$lang['openvpn_plugin_app_name'] = 'VPN খুলুন নীতিসমূহ';
+$lang['openvpn_plugin_pptp_server_user'] = 'VPN খুলুন নিচে';
