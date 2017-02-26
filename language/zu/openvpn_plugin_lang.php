20fa9430692b1999a440b4cc6400f445d34f7b63
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:58:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN Izinqubomgomo ukunikeza control ukufinyelela OpenVPN iseva lokusebenza.';
+$lang['openvpn_plugin_app_name'] = 'OpenVPN Izinqubomgomo';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN Umsebenzisi';
