18d137cc23c4a7284e3a505a225a09d791f9a9d9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:58:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'אָפּענוופּן פּאַלאַסיז צושטעלן צוטריט קאָנטראָל פֿאַר די אָפּענוופּן סערווער אַפּ.';
+$lang['openvpn_plugin_app_name'] = 'אָפּענוופּן פּאָליסיעס';
+$lang['openvpn_plugin_pptp_server_user'] = 'אָפּענוופּן אַוואַטאַר';
