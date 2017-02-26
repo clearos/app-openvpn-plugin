ee7adb532de0db99e4f091c99f67587f7cab4329
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:53:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN Бодлого OpenVPN серверийн програм нь хандалтын хяналтыг олгодог.';
+$lang['openvpn_plugin_app_name'] = 'OpenVPN бодлого';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN хэрэглэгчийн';
