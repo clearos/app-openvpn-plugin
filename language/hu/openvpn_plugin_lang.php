d3d1ec446f27485e835f1682451d356e3d18d2bf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:50:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN házirendje eléréskorlátozást az OpenVPN szerver alkalmazás.';
+$lang['openvpn_plugin_app_name'] = 'OpenVPN házirendje';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN felhasználó';
