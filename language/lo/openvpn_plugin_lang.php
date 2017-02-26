59dd6c12d568a809a7172f7970c22e870c95600a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:52:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'ນະໂຍບາຍ OpenVPN ໃຫ້ການຄວບຄຸມການເຂົ້າເຖິງສໍາລັບ app server OpenVPN.';
+$lang['openvpn_plugin_app_name'] = 'ນະໂຍບາຍ OpenVPN';
+$lang['openvpn_plugin_pptp_server_user'] = 'User OpenVPN';
