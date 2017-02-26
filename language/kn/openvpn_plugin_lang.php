ba9a1491e21a45eff8a247721b7439ed975f265a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:52:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'VPN ತೆರೆಯಿರಿ ನೀತಿಗಳು VPN ತೆರೆಯಿರಿ ಸರ್ವರ್ ಅಪ್ಲಿಕೇಶನ್ ಆಕ್ಸೆಸ್ ಕಂಟ್ರೋಲ್ ನೀಡಲಾಗುತ್ತದೆ.';
+$lang['openvpn_plugin_app_name'] = 'VPN ತೆರೆಯಿರಿ ನೀತಿಗಳು';
+$lang['openvpn_plugin_pptp_server_user'] = 'VPN ತೆರೆಯಿರಿ ಬಳಕೆದಾರ';
