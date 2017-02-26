f4501b686b954f870cb054da9f759e58de5c78c2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:53:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN നയങ്ങള് VPN തുറക്കുക സെർവർ അപ്ലിക്കേഷൻ ആക്സസ്സ് നിയന്ത്രണം നൽകുന്നു.';
+$lang['openvpn_plugin_app_name'] = 'OpenVPN നയങ്ങള്';
+$lang['openvpn_plugin_pptp_server_user'] = 'VPN തുറക്കുക ഉപയോക്തൃ';
