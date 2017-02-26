b3e6e209a10353b34d687b2a584a4c31e00667e2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:58:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN Chính sách cung cấp kiểm soát truy cập cho các ứng dụng máy chủ OpenVPN.';
+$lang['openvpn_plugin_app_name'] = 'Chính sách OpenVPN';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN tài';
