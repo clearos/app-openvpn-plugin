5fa9eefeffb6b0f4d2a99505e2a077bdec716917
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:56:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN విధానాలు OpenVPN సర్వర్ అనువర్తనం ప్రాప్యత నియంత్రణను అందిస్తాయి.';
+$lang['openvpn_plugin_app_name'] = 'OpenVPN విధానాలు';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN వాడుకరి';
