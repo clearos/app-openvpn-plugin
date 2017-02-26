638ad230ed593b9de4aba09bc0fbae1ead6d71bc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:56:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN Kawijakan nyadiakeun kadali aksés pikeun app server OpenVPN.';
+$lang['openvpn_plugin_app_name'] = 'Kawijakan OpenVPN';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN pamaké';
