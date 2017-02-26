c0ebddff9ef740d4257ebd93a943a76297846bc9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:47:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'توفر سياسات المسنجر التحكم في الوصول لهذا التطبيق خادم OpenVPN.';
+$lang['openvpn_plugin_app_name'] = 'سياسات المسنجر';
+$lang['openvpn_plugin_pptp_server_user'] = 'المسنجر العضو';
