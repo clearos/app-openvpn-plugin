d86ceda522905fa6de829d16c92cf62c068e339e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:55:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'Siyaasadaha OpenVPN bixiyaan helitaanka gacanta for app server OpenVPN ah.';
+$lang['openvpn_plugin_app_name'] = 'Siyaasadaha OpenVPN';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN User';
