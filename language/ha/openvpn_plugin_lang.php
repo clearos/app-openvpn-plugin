b6f4453e10203556de2b7bcd97707f393353006d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:50:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN Asiri samar da damar iko ga OpenVPN uwar garken app.';
+$lang['openvpn_plugin_app_name'] = 'OpenVPN Asiri';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN User';
