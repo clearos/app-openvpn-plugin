10e6477e8fa57541f2e80fefe2c9b9bf4d5007ef
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:57:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'Сиёсати OpenVPN таъмини идоракунии дастрасӣ, барои барнома сервер OpenVPN.';
+$lang['openvpn_plugin_app_name'] = 'сиёсати OpenVPN';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN корбар';
