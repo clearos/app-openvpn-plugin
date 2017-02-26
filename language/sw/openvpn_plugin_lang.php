0bc19c24689f2f60091d36e719ec6513d10eee47
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:56:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN Sera kutoa upatikanaji wa kudhibiti kwa programu server OpenVPN.';
+$lang['openvpn_plugin_app_name'] = 'OpenVPN Sera';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN mtumiaji';
