6552fdc48ae4f0eb899f42fe2dcdd092c412914a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:48:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN Ndondomeko imapatsa kulamulira kwa app OpenVPN Seva.';
+$lang['openvpn_plugin_app_name'] = 'OpenVPN Ndondomeko';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN Wosuta';
