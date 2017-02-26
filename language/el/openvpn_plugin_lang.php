6a9134138f833c7774defcb5fe8cc86f94edccdb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:50:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN Πολιτικές παρέχει έλεγχο πρόσβασης για την εφαρμογή εξυπηρετητή OpenVPN.';
+$lang['openvpn_plugin_app_name'] = 'Πολιτικές OpenVPN';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN χρήστη';
