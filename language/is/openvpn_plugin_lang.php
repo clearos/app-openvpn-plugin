cec5997a0602a5a62196dea29d2d9385e2e3eaac
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:51:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN Reglur veita aðgang stjórna fyrir OpenVPN miðlara app.';
+$lang['openvpn_plugin_app_name'] = 'OpenVPN Reglur';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN User';
