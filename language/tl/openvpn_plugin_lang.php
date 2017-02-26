c1f7332f63107b1e4298b792adf57a825658edbb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:57:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN Patakaran magbigay ng access control para sa OpenVPN server app.';
+$lang['openvpn_plugin_app_name'] = 'Mga Patakaran OpenVPN';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN User';
