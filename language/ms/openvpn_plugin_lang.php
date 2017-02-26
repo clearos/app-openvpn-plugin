39da1db68595ac99658ec9d4f998296db6758c55
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:53:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'Polisi OpenVPN menyediakan kawalan akses untuk aplikasi pelayan OpenVPN.';
+$lang['openvpn_plugin_app_name'] = 'Polisi OpenVPN';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN Pengguna';
