8bfa98607f9fbd9c82e69f9682fab37e12709bad
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:55:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN ප්රතිපත්ති මෙම OpenVPN සේවාදායකය යෙදුම සඳහා ප්රවේශ පාලක ලබා දෙයි.';
+$lang['openvpn_plugin_app_name'] = 'OpenVPN ප්රතිපත්ති';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN පරිශීලක';
