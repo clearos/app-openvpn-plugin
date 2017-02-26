61629d5aa02ecd64d807f6d2a5879418b24e9506
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:51:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN Atumatu-enye ohere akara maka OpenVPN nkesa na-eji ngwa.';
+$lang['openvpn_plugin_app_name'] = 'OpenVPN atumatu';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN User';
