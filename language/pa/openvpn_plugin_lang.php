7cd3031790da7c3112e96ac596431c2dd93f5bfa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:54:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'ਦਿੱਤਾ ਡਰਾਇਰ ਦਿੱਤਾ ਸਰਵਰ ਐਪ ਲਈ ਪਹੁੰਚ ਕੰਟਰੋਲ ਮੁਹੱਈਆ ਕਰਦਾ ਹੈ.';
+$lang['openvpn_plugin_app_name'] = 'ਦਿੱਤਾ ਡਰਾਇਰ';
+$lang['openvpn_plugin_pptp_server_user'] = 'ਦਿੱਤਾ ਯੂਜ਼ਰ';
