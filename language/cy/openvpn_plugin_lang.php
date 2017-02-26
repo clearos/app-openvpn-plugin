fec088520490ac94cbeeecbd6faedf67dc7efcac
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:58:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'Polisïau OpenVPN darparu rheolaeth mynediad ar gyfer y app gweinydd OpenVPN.';
+$lang['openvpn_plugin_app_name'] = 'Polisïau OpenVPN';
+$lang['openvpn_plugin_pptp_server_user'] = 'Defnyddiwr OpenVPN';
