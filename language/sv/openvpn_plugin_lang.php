263cb17dd3f9aa43282eb760608fff2340e36aac
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:56:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN Policies ger åtkomstkontroll för OpenVPN server app.';
+$lang['openvpn_plugin_app_name'] = 'OpenVPN Policies';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN Användar';
