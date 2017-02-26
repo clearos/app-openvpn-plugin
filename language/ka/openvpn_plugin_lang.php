2e31aca748a85a166e7ad5c238d2ba6c954b1be9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:51:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/openvpn_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['openvpn_plugin_app_description'] = 'OpenVPN პოლიტიკის უზრუნველყოს კონტროლის OpenVPN სერვერზე app.';
+$lang['openvpn_plugin_app_name'] = 'OpenVPN პოლიტიკა';
+$lang['openvpn_plugin_pptp_server_user'] = 'OpenVPN მომხმარებელი';
